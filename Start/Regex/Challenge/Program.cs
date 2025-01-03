1cf3f40817604073928c075e4d00ecaf9cd1bf08
Edwin Choate <edwin.choate@pm.me>
2025-01-03 17:25:32 -0500

    completes the course

---
+++ Start/Regex/Challenge/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Challenge;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(ReverseDate("11/12/2020"));
+    }
+
+    public static string ReverseDate (string date) 
+    {
+        Regex digits = new Regex("[0-9]+");
+
+        MatchCollection matches = digits.Matches(date);
+
+        return String.Join('-', new string[] { matches[2].ToString(), matches[0].ToString(), matches[1].ToString() });
+    }
+}
