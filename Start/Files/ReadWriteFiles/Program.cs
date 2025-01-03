62b981b29bfa67d389924533794e70b0c5ec33cb
Edwin Choate <edwin.choate@pm.me>
2025-01-03 10:55:39 -0500

    4.2 Reading and writing files

--- Start/Files/ReadWriteFiles/Program.cs
+++ Start/Files/ReadWriteFiles/Program.cs
@@ -5,15 +5,21 @@
 const string filename = "TestFile.txt";
 
 // TODO 1: WriteAllText overwrites a file with the given content
-if (!File.Exists(filename)) {
-
-}
+// if (!File.Exists(filename)) {
+    File.WriteAllText(filename, "This content replaces everything in a TXT file.");
+// }
 
 // TODO 3: AppendAllText adds text to an existing file
-
+File.AppendAllText(filename, " This text is appended to the file.");
 
 // TODO 4: A FileStream can be opened and written to until closed
-
+using (StreamWriter sw = File.AppendText(filename)) 
+{
+    sw.WriteLine();
+    sw.WriteLine("line 1");
+    sw.WriteLine("line 2");
+    sw.WriteLine("line 3");
+}
 
 // TODO 2: ReadAllText reads all the content from a file
-
+Console.WriteLine(File.ReadAllText(filename));
